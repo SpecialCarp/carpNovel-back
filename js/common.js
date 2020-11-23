f790f23efcfd412ceb31fd9758135e0b1871abd5
SpecialCarp <sc63482115@163>
2020-11-23 20:45:32 +0800

    修改验证路径

--- js/common.js
+++ js/common.js
@@ -1,5 +1,5 @@
 function adminInterceptor(adminName,adminPassword){
-    $.post("http://127.0.0.1/CarpNovel/back/index", {
+    $.post("http://127.0.0.1/carpNovel/back/index", {
         account:adminName,
         password:adminPassword, 
         function (result) {
